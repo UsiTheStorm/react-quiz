5f959a3dc30f83098b25953f258264c2c155dc76
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-08-25 15:08:39 +0100

    Add fetching datat and reducer state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,59 @@
-import { useState } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 import './App.css';
 
-import questions from './questions.json';
+// import questions from './questions.json';
 import Header from './components/Header';
 import Main from './components/Main';
 
+const initialState = {
+  questions: [],
+
+  // 'loading' | 'error' | 'ready' | 'active' | 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFaild':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Unknown action');
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch('http://localhost:3001/questions');
+
+        if (!res.ok) {
+          throw new Error('Failed to fetch questions');
+        }
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+        console.log(data);
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+        console.error(err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header />
