a810018f72b72e6566c67cd3fb6d48fd07f6f89d
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-08-27 11:03:04 +0100

    Implement Next button component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Loader from './components/Loader';
 import Error from './components/Error';
 import StartScreen from './components/StartScreen';
 import Question from './components/Question';
+import NextButton from './components/NextButton';
 
 const initialState = {
   questions: [],
@@ -48,6 +49,11 @@ function reducer(state, action) {
           action.payload === question.correctOption ? state.points + question.points : state.points,
       };
     }
+    case 'nextQuestion':
+      return {
+        ...state,
+        index: state.index + 1,
+      };
     default:
       throw new Error('Unknown action');
   }
@@ -91,6 +97,7 @@ function App() {
         {status === 'active' && (
           <Question question={questions[index]} dispatch={dispatch} answer={answer} />
         )}
+        <NextButton dispatch={dispatch} answer={answer} />
       </Main>
     </>
   );
